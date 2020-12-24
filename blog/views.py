f37a8cdd141b80e7d8ccb5595eda02a497bae88c
shatrix1217 <53997721+shatrix1217@users.noreply.github.com>
2020-12-25 03:59:38 +0900

    add base template

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author' : 'Watabe',
+        'title' : 'Dummy post 1',
+        'content' : 'first post content',
+        'date_posted' : 'Dec 25, 2020'
+    },
+    {
+        'author' : 'Watabe',
+        'title' : 'Dummy post 2',
+        'content' : 'second post content',
+        'date_posted' : 'Dec 25, 2020'
+    }
+]
+
 # Create your views here.
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts' : posts
+    }
+    return render(request, 'blog/home.html',context)
 
 
 def about(request):
