596b11a65f8a6b0c5dd8f034fd3f1419fc4851f6
shatrix1217 <53997721+shatrix1217@users.noreply.github.com>
2021-01-09 19:30:27 +0900

    Added django-heroku

--- first_webapp/settings.py
+++ first_webapp/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import os 
+import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3@sox!c+6vt&7hlfn^x5fes#&meb$a0mfp-o3$(363w@3227#='
+SECRET_KEY = os.environ.get('Django_SecretKey')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://sk-django-blog.herokuapp.com/']
 
 
 # Application definition
@@ -138,3 +139,5 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+
+django_heroku.settings(locals())
